ae765ea2a8e189d188c02f7bd0d02fa6e68a892b
Дмитрий Саенко <saenkodmitriiol@gmail.com>
2025-11-19 14:58:31 +0300

    времено убрал аутентификацию

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,9 +1,6 @@
 package middleware
 
 import (
-	"net/http"
-
-	"github.com/bytedance/gopkg/util/logger"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/sessions"
 )
@@ -11,14 +8,14 @@ import (
 var Store *sessions.CookieStore
 
 func RequireAuth(c *gin.Context) {
-	session, _ := Store.Get(c.Request, "vpn-session")
+	// session, _ := Store.Get(c.Request, "vpn-session")
 
-	auth, ok := session.Values["authenticated"].(bool)
-	if !ok || !auth {
-		logger.Infof("Redirecting to login: %s", c.Request.URL.Path)
-		c.Redirect(http.StatusFound, "/login")
-		return
-	}
-	logger.Infof("Authenticated request: %s", c.Request.URL.Path)
+	// auth, ok := session.Values["authenticated"].(bool)
+	// if !ok || !auth {
+	// 	logger.Infof("Redirecting to login: %s", c.Request.URL.Path)
+	// 	c.Redirect(http.StatusFound, "/login")
+	// 	return
+	// }
+	// logger.Infof("Authenticated request: %s", c.Request.URL.Path)
 	c.Next()
 }
